61945de24ac4fb697f615fef24b53eeb2737f515
Sansnn <905558609@qq.com>
2022-03-09 16:22:23 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//---------------------------------------------------------------------
+// github        https://github.com/noctcloud/noctjs
+// organization  Noct Cloud
+// author        Sansnn
+// license       MIT
+//---------------------------------------------------------------------
+
+"use strict";
+
+module.exports = {
+	run: require("./program/run"),
+	service: require("./abstracts/service"),
+	filter: require("./abstracts/filter")
+}
